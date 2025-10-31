92f13bd35fe669e3982c30de211c17a8415c6d94
rivitaly <158235739+rivitaly@users.noreply.github.com>
2025-10-30 19:18:41 -0600

    City Data Implementation
    Using imported data from OpenWeather, was able to successfully create a function to provide a randomized city along with its other important data.

--- js/cities.js
+++ js/cities.js
@@ -1,22 +1,30 @@
-export const cityNames = [
-  "Ottawa",        
-  "Edmonton",
-  "Victoria",
-  "Winnipeg",
-  "Fredericton",
-  "St. John's",
-  "Halifax",
-  "Toronto",
-  "Charlottetown",
-  "Québec City",
-  "Regina",
-  "Yellowknife",
-  "Iqaluit",
-  "Whitehorse"
-];
+let cities = null;
+fetch('../assets/city.list.json') //This JSON is provided at https://openweathermap.org/current#cityid
+.then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+})
+.then(data => {
+  cities = data;
+})
 
-//city names file
-//we can add a whole bunch of cities, only issue is for some places we will need extra info: states need zip-code
-//all info rehard how to make API calls can be found:
-//https://openweathermap.org/current
-//https://openweathermap.org/current#geocoding 
+//Function returns a table providing the city name, country name, longitude and latitude
+//Need to make accessible from main.js
+function getRandomCity() {
+  let cityName = cities[Math.floor(Math.random() * cities.length)];
+  let countryFormatter = new Intl.DisplayNames(['en'], {type: 'region'});
+  let city = cityName.name;
+  let countryCode = cityName.country
+  let country = countryFormatter.of(countryCode)
+  let long = cityName.long;
+  let lat = cityName.lat;
+  return {city, country, long, lat};
+}
+
+//Testing outputs
+const button = document.getElementById("start-button")
+button.addEventListener('click', () => {
+  let chosenCity = getRandomCity();
+  console.log(`${chosenCity.city}, ${chosenCity.country}`)
+})
