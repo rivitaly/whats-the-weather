1286597980c14620feae2374ba6f7912f745bc2d
GumdropNinja <cartergundrum@gmail.com>
2025-11-04 15:47:52 -0600

    Reformatted Weather Class
    Combined Weather and Cities into a single class to follow the MVC architecture. This class will be our model. The weather object contains all the information that we will need for the game.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import {getRandomCity} from './cities.js'
 import Weather from './weather.js'
 
 //Variables
@@ -11,7 +10,7 @@ const locationDisplay = document.getElementById("location-display");
 //Events
 const button = document.getElementById("start-button")
 button.addEventListener('click', async () => {
-  const {city, country, long, lat} = getRandomCity(); //Assigns to variables without needing to set additional local var
-  await weather.setWeatherData(long, lat); //Call weather.type to access the weather classification
-  locationDisplay.innerHTML = `${city}, ${country}`;
+  await weather.init(); //Call weather.type to access the weather classification
+  locationDisplay.innerHTML = `${weather.city}, ${weather.country}`;
+  console.log(weather.main);
 })
