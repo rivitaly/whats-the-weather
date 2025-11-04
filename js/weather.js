9b57120ab045d0c27871ea86f777d8539cb8d750
GumdropNinja <cartergundrum@gmail.com>
2025-11-04 15:47:52 -0600

    Reformatted Weather Class
    Combined Weather and Cities into a single class to follow the MVC architecture. This class will be our model. The weather object contains all the information that we will need for the game.

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,57 @@
+
+var api_key = "XXXXXXXXXXXXXXXXXXXXXXXXX" //this is Carter's key for the free version of OpenWeatherMapAPI
+
 //Weather Class System 
 //Note: will need to adjust the class to follow the design models discussed in class
 export class WeatherAPISystem {
-
-    api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" //this is Carter's key for the free version of OpenWeatherMapAPI
     type = null;    //update this value, then can access as weather.type from main.js
+    cities = [];
+    city = null;
+    countryCode = null;
+    country = null;
+    long = null;
+    lat = null;
+    main = null;     //contains temp, feels_like, etc
 
     async init() {
+        await this.getCityData();
+        await this.getRandomCity();
         await this.setWeatherData();
     }
-    
+
     //Sends a fetch to return the weather data from OpenWeatherAPI
     async setWeatherData(long, lat) {
-        let Forecast_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${this.api_key}`;
+        let Forecast_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.long}&appid=${api_key}`;
         try {
             const res = await fetch(Forecast_URL);
             const data = await res.json();
             this.type = data.weather[0].main;
+            this.main = data.main;
         }
         catch {
             alert('Failed to get current weather');
         }
+    }
+
+    async getCityData() {
+        try {
+            const res = await fetch('./assets/city.list.json') //This JSON is provided at https://openweathermap.org/current#cityid
+            const data = await res.json();
+            this.cities = data;
+        }
+        catch {
+            alert('Failed to get city data')
+        }
+    }
+
+    async getRandomCity() {
+        let cityName = this.cities[Math.floor(Math.random() * this.cities.length)];
+        let countryFormatter = new Intl.DisplayNames(['en'], { type: 'region' });
+        this.city = cityName.name;
+        this.countryCode = cityName.country
+        this.country = countryFormatter.of(this.countryCode)
+        this.long = cityName.coord.lon;
+        this.lat = cityName.coord.lat;
     }
 
 }
