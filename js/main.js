9a907716a74c1bcf8e76c28af6fa46a1e733e9a9
rivitaly <158235739+rivitaly@users.noreply.github.com>
2025-11-08 14:55:13 -0600

    Core Game Logic Implementation
    I have created the core game logic to make the system work currently without an account system.  I have added buttons to represent the weather types to guess and they have colour responsiveness to indicate correctness.  Buttons are not permanent placement and only used for ensuring the game logic is in place.  Additionally fixed setWeatherData() to accurately set the Atmosphere weather type.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,74 @@
 import Weather from './weather.js'
 
 //Variables
+let matchStarted = false, guessed = false;
 let city, country, long, lat = null;
 const weather = new Weather();
 
 //Elements
-const locationDisplay = document.getElementById("location-display");
+const locationDisplay = document.getElementById("location-title");
+const mainButton = document.getElementById("main-button")
+const weatherButtons = document.getElementById("weather-button-container");
+
+//Functions
+function correct(button){
+  //Visual Update
+  button.classList.add("correct")
+
+  //Database Update
+}
+
+function incorrect(button, correctButton){
+  //Visual Update
+  button.classList.add("incorrect");
+  correctButton.classList.add("correct");
+
+  //Database Update
+}
 
 //Events
-const button = document.getElementById("start-button")
-button.addEventListener('click', async () => {
+mainButton.addEventListener('click', async () => {
+  //Start Game
   await weather.init(); //Call weather.type to access the weather classification
   locationDisplay.innerHTML = `${weather.city}, ${weather.country}`;
-  console.log(weather.main);
+  if (!matchStarted)
+    matchStarted = true;  //Designed so it only starts when player clicks start everytime page opens
+  else
+  {
+    //Clear all correct/incorrect visuals
+    document.querySelectorAll(".weather-button").forEach(button => {
+      button.classList.remove("correct", "incorrect");
+    });
+
+    //Reset guess
+    guessed = false;
+  }
+  //Hide Start Button, Unhide guess buttons
+  //Opted for *.style.visibility over .hidden so elements dont move,
+  //Potential change to make: convert to id to animate fade in/out
+  mainButton.style.visibility = "hidden";
+  weatherButtons.style.visibility = "visible";
+  mainButton.innerHTML = "Next Round";
 })
+
+document.querySelectorAll(".weather-button").forEach(button => {
+  button.addEventListener('click', async => {
+    //Only consider button inputs when round is going
+    if (!matchStarted || guessed) { return; }
+    guessed = true;
+    console.log("Weather: " + weather.type)
+
+    if (button.id === weather.type) {
+      //Correct guess
+      correct(button)
+    }
+    else {
+      //Incorrect guess
+      var correctButton = document.getElementById(weather.type);
+      incorrect(button, correctButton);
+    }
+
+    //Show Next Round button
+    mainButton.style.visibility = "visible";
+  })
+});
