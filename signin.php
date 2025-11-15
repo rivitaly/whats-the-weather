b009be6ee74e8f5907690c82f422d0e16e943c0f
GumdropNinja <cartergundrum@gmail.com>
2025-11-15 12:32:17 -0600

    FIX: White on white text
    Text should be black now

--- signin.php
+++ signin.php
@@ -108,7 +108,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { //post form from sign in
             //print if any errors 
             if (!empty($errors)) {
                 foreach ($errors as $type => $message) {
-                    print ("$type: $message \n<br />");
+                    echo '<p class="messages">'.$type . ':' . $message."\n</p";
                 }
             }
             ?>
