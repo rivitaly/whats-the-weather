15a07aded2aba9bd8013e778d7608d7d2953c64f
GumdropNinja <cartergundrum@gmail.com>
2025-11-15 16:31:15 -0600

    Fixed Issue
    can't have a db stored in the session array. Need to have the db for moderator so we will pass it in as a parameter.

--- accountFactory.php
+++ accountFactory.php
@@ -3,14 +3,12 @@ require_once("db.php");
 
 class Account
 {
-    protected $db;
     public $id;
     public $username;
     public $role;
 
-    public function __construct($n_db, $n_id, $n_username, $n_role)
+    public function __construct($n_id, $n_username, $n_role)
     {
-        $this->db = $n_db;
         $this->id = $n_id;
         $this->username = $n_username;
         $this->role = $n_role;
@@ -24,10 +22,10 @@ class Player extends Account
 
 class Moderator extends Account
 {
-    public function ban($ban_username)
+    public function ban($db, $ban_username)
     {
         // look for the username 
-        $result = $this->db->query("SELECT username FROM accounts WHERE username='$ban_username'");
+        $result = $db->query("SELECT username FROM accounts WHERE username='$ban_username'");
         $match = $result->fetch();
         //ban
     }
@@ -35,14 +33,14 @@ class Moderator extends Account
 
 class AccountFactory
 {
-    public static function createAccount($n_db, $n_id, $n_username, $n_role)
+    public static function createAccount($n_id, $n_username, $n_role)
     {
         switch ($n_role) {
             case "Moderator":
-                return new Moderator($n_db, $n_id, $n_username, $n_role);
+                return new Moderator( $n_id, $n_username, $n_role);
 
             case "Player":
-                return new Player($n_db, $n_id, $n_username, $n_role);
+                return new Player($n_id, $n_username, $n_role);
         }
     }
 }
