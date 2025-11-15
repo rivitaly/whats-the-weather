999c5467ba43b9b0a8e4e9e3a8fb6e4d7583a04e
GumdropNinja <cartergundrum@gmail.com>
2025-11-15 17:44:07 -0600

    Start of more php files

--- insertGuessScript.php
+++ insertGuessScript.php
@@ -1,6 +1,16 @@
 <?php
-require_once("db.php")
+require_once("db.php");
+session_start();
 
+if ($_SESSION["account"]->username != "")
+try { // data base connection
+        $db = new PDO($attr, $db_user, $db_pwd, $options);
+    } catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int) $e->getCode());
+    }
 
+    // look for if the username already exists
+    $result = $db->query("SELECT username FROM accounts WHERE username='$username'");
+    $match = $result->fetch();
 
 ?>
