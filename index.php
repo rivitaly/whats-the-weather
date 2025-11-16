d532421858df2a36d903d8103be93012f52379c5
MrToast72 <mrtoast72@gmail.com>
2025-11-16 11:59:34 -0600

    Changed the navigation menu to a hamburger style menu. This will allow us to scale to mobile much easier down the line. Adjusted all webpages to match styling.

--- index.php
+++ index.php
@@ -45,31 +45,41 @@ else
 </head>
 
 <body>
-  <header>
-    <div class="header">
-      <a href="index.php">
-        <h1 id="header-title">What's the Weather</h1>
-      </a>
-      <nav>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <?php
-            if (isset($_SESSION["account"])){
-              if (isset($_SESSION["role"]) && $_SESSION["role"] === "Moderator"){
-                echo '<li><a href="mod.php">Mod Panel</a></li>';
-              }
-              echo '<li><a href="stats.php">Player Stats</a></li>';
-              echo '<li><a href="logout.php">Log Out</a></li>';
+<header>
+  <div class="header">
+    <a href="index.php" class="logo-link">
+      <h1 id="header-title">What's the Weather</h1>
+    </a>
+
+    <!-- Hamburger Button -->
+    <input type="checkbox" id="nav-toggle" class="nav-toggle">
+    <label for="nav-toggle" class="hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+
+    <!-- Dropdown Menu -->
+    <nav class="nav-menu">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <?php
+          if (isset($_SESSION["account"])){
+            if (isset($_SESSION["role"]) && $_SESSION["role"] === "Moderator"){
+              echo '<li><a href="mod.php">Mod Panel</a></li>';
             }
-            else{
-              echo '<li><a href="signin.php">Sign In</a></li>';
-              echo '<li><a href="signup.php">Sign Up</a></li>';
-            }
-          ?>
-        </ul>
-      </nav>
-    </div>
-  </header>
+            echo '<li><a href="stats.php">Player Stats</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="signin.php">Sign In</a></li>';
+            echo '<li><a href="signup.php">Sign Up</a></li>';
+          }
+        ?>
+      </ul>
+    </nav>
+  </div>
+</header>
   <div id="welcome-user">
     <?php echo "<p class='welcome-message'>{$welcome_message}</p>"?>
   </div>
