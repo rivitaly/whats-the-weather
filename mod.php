f5fa4675cbb94b8b8c70e0536cf34792ad8a6e85
rivitaly <158235739+rivitaly@users.noreply.github.com>
2025-11-15 18:54:51 -0600

    Basic Session Requirements
    Cannot access certain pages if not logged in or correct account type

--- mod.php
+++ mod.php
@@ -2,5 +2,49 @@
 require_once("db.php");
 session_start();
 
+//If not logged in or not Moderator, redirect
+if(!isset($_SESSION["account"]) || !isset($_SESSION["role"]) || $_SESSION["role"] !== "Moderator"){
+    header("Location: index.php");
+    exit();
+}
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>What's the Weather</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <header>
+    <div class="header">
+      <a href="index.php">
+        <h1 id="header-title">What's the Weather</h1>
+      </a>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <?php
+            if (isset($_SESSION["account"])){
+              echo '<li><a id="logout" href="logout.php">Log Out</a></li>';
+            }
+          ?>
+          <?php
+            if (!isset($_SESSION["account"])){
+              echo '<li><a href="signin.php">Sign In</a></li>';
+              echo '<li><a href="signup.php">Sign Up</a></li>';
+            }
+          ?>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <script type="module" src="js/main.js"></script>
+  <script type="module" src="js/render.js"></script>
+  </div>
+</body>
+
+</html>
