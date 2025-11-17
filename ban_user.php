d37e02d817c9e21adbf721bdeb84c2d84e9228ad
cwrightca <wfamily59@gmail.com>
2025-11-17 15:51:36 -0600

    Update ban_user.php

--- ban_user.php
+++ ban_user.php
@@ -19,29 +19,28 @@ else if (!$playerAccount) {
 } else {
     try {
         $db = new PDO($attr, $db_user, $db_pwd, $options);
+
+        $result = $db->query("SELECT banned from accounts WHERE account_id = '$playerAccount'");
+        // Checking for Database Errors
+        if (!$result) {
+            $db = null;
+            header("Location: index.php");
+            // Ban Function
+        } else if ($row = $result->fetch()) {
+            // Swaps the values between 0 and 1 which represents banned and unbanned, 0 being unbanned and 1 being banned
+            $banUnban = $row['banned'] ? '0' : '1';
+            // Updates the database
+            $db->exec("UPDATE accounts SET banned = '$banUnban' WHERE account_id = '$playerAccount'");
+            // Returns you to the Moderator Page
+            header("Location: mod.php");
+            exit();
+        }
     } catch (PDOException $e) {
         throw new PDOException($e->getMessage(), (int) $e->getCode());
     }
-    $result = $db->query("SELECT banned FROM accounts WHERE account_id = '$playerAccount'");
-    // Checking for Database Errors
-    if (!$result) {
-        $db = null;
-        header("Location: index.php");
-    // Ban Function
-    } else if ($row = $result->fetch()) {
-        // Swaps the values between 0 and 1 which represents banned and unbanned, 0 being unbanned and 1 being banned
-        $banUnban = $row['banned'] ? 1 : 0;
-        // Updates the database
-        $db->exec("UPDATE accounts SET banned = '$banUnban' WHERE account_id = '$playerAccount'");
-        // Returns you to the Moderator Page
-        $db = null;
-        header("Location: mod.php");
-        exit();
-        }
+    $db = null;
 }
+
 // In the event of some unknown error, Redirects you back to the main page
 header("Location: index.php")
 ?>
-
-
-
