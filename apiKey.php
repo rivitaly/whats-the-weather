acc02b3604cff605f79b71d3217a55a721b987a2
MrToast72 <mrtoast72@gmail.com>
2025-11-17 21:16:50 -0600

    api key fix

--- apiKey.php
+++ apiKey.php
@@ -1,5 +1,11 @@
 <?php
-define("API_KEY", getenv("API_KEY"));
+$apiKey = getenv("API_KEY");
+
+if (!$apiKey) {
+    throw new Exception("API_KEY environment variable not set!");
+}
+
+define("API_KEY", $apiKey);
 
 echo json_encode(["api_key" => API_KEY]);
 
