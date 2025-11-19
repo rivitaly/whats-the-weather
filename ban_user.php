49a23a2ecc1757d75f33ac1823f82778bf2c082d
cwrightca <wfamily59@gmail.com>
2025-11-18 21:53:31 -0600

    Formatted and removed old ban function that was moved to accountFactory

--- ban_user.php
+++ ban_user.php
@@ -22,30 +22,13 @@ else if (!$_POST['account_id']) {
         $db = new PDO($attr, $db_user, $db_pwd, $options);
         $playerAccount = $_POST['account_id'];
         $_SESSION["account"]->ban($db, $playerAccount); //Moderator check earlier allows us to call ban()
-        /*
-        $result = $db->query("SELECT banned from accounts WHERE account_id = '$playerAccount'");
-        // Checking for Database Errors
-        if (!$result) {
-            $db = null;
-            header("Location: index.php");
-            // Ban Function
-        } else if ($row = $result->fetch()) {
-            // Swaps the values between 0 and 1 which represents banned and unbanned, 0 being unbanned and 1 being banned
-            $banUnban = $row['banned'] ? '0' : '1';
-            // Updates the database
-            $db->exec("UPDATE accounts SET banned = '$banUnban' WHERE account_id = '$playerAccount'");
-            // Returns you to the Moderator Page
-            header("Location: mod.php");
-            exit();
-        }
-        */
     } catch (PDOException $e) {
         throw new PDOException($e->getMessage(), (int) $e->getCode());
     }
     $db = null;
 }
-
 // In the event of some unknown error, Redirects you back to the main page
 header("Location: index.php")
 ?>
+
 
